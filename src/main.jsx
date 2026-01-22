6668b5412727a145fdaa87ccbef7df9e84c978f7
Ritin rawat <ritinrawat85@gmail.com>
2026-01-22 12:45:15 +0530

    push

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext.jsx'
 import CaptainContext from './context/CaptainContext.jsx'
 import SocketContext from './context/SocketContext.jsx'
+import { HashRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
     <CaptainContext>
     <UserContext>
    <SocketContext>
@@ -18,6 +20,6 @@ createRoot(document.getElementById('root')).render(
 
     </UserContext>
     </CaptainContext>
-    
+    </HashRouter>
     </StrictMode>,
 )
